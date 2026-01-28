945b177c56fea558db4a048b20409151b6aa14a3
x12w <w17802627260@gmail.com>
2026-01-29 01:35:20 +0800

    fix:切换回较稳定的25.11版本

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "x12w's nixos configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
   };
 
   outputs = { self, nixpkgs }@inputs: {

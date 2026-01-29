aea87f43246e9a84525f4570e833dfae3847ff93
x12w <w17802627260@gmail.com>
2026-01-30 00:49:54 +0800

    fix:修改入口参数

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     daed.url = "github:daed-revived/daed";
   };
 
-  outputs = { self, nixpkgs, dead }@inputs: {
+  outputs = { self, nixpkgs, daed }@inputs: {
 
     nixosConfigurations.x12w-nix = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";

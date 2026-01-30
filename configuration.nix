156d2e036fa4ea92410a650b5fa166624b6c2319
x12w <w17802627260@gmail.com>
2026-01-30 20:18:07 +0800

    feat(config):启用ssh

--- configuration.nix
+++ configuration.nix
@@ -248,6 +248,13 @@
 
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
+  services.openssh = {
+  enable = true;
+  settings = {
+    PasswordAuthentication = true; # 启用密码登录
+    PermitRootLogin = "no";        # 禁止 root 直接登录
+  };
+};
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];

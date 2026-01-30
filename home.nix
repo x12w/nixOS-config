c952badb97daf79e63de048c1c1f773ba25ea6a0
x12w <w17802627260@gmail.com>
2026-01-30 20:09:09 +0800

    feat(home):迁移部分配置到home.nix

--- home.nix
+++ home.nix
@@ -19,4 +19,20 @@
 
   # 设置 Home Manager 状态版本（建议保持与系统安装时一致）
   home.stateVersion = "25.11";
+
+  # 设置输入法环境变量
+  home.sessionVariables = {
+  GTK_IM_MODULE = ""; # 对应你 configuration.nix 中的 lib.mkForce ""
+  QT_IM_MODULE = "";
+  };
+
+  # 配置git
+  programs.git = {
+  enable = true;
+  userName = "x12w";
+  userEmail = "w17802627260@gmail.com";
+  extraConfig = {
+    init.defaultBranch = "main";
+  };
+};
 }

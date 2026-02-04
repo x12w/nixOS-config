242cd3001a983f39403fe5fafafccc795e4f73b5
x12w <w17802627260@gmail.com>
2026-02-04 22:43:20 +0800

    fix(home):修正软件包配置

--- home.nix
+++ home.nix
@@ -177,9 +177,6 @@
 
       # 其他通用工具
       nodePackages.typescript-language-server
-
-      jetbrains.clion
-      feishu
     ];
   };
 
@@ -192,6 +189,9 @@
     catppuccin-kde          # 提供全局主题、色彩方案和窗口装饰
     catppuccin-papirus-folders # 提供配套图标
     bibata-cursors
+
+    jetbrains.clion
+    feishu
 
     # --- NUR ---
     nur.repos.xddxdd.baidunetdisk

1e53944b6a5cbd51a0a422ff7f7818e27fa0debb
x12w <w17802627260@gmail.com>
2026-02-11 13:30:18 +0800

    feat(home):修改主题配置

--- home.nix
+++ home.nix
@@ -210,13 +210,13 @@
 
   catppuccin = {
     flavor = "mocha"; # 选择你喜欢的：latte, frappe, macchiato, mocha
-    accent = "sapphire";
+    accent = "blue";
     enable = true;    # 这会尝试为所有支持的 program.* 开启主题
 
     cursors.enable = false; # 光标主题
     fcitx5.enable = true;
     kvantum.enable = true;
-    kvantum.apply = true;
+    kvantum.apply = false;
   };
 
   /*

a4562570522435f77a347ccddbadcb6beda07c04
x12w <w17802627260@gmail.com>
2026-03-07 00:50:02 +0800

    feat(config):更新内核版本至6.19,并配置显卡驱动

--- configuration.nix
+++ configuration.nix
@@ -57,7 +57,7 @@ in
   boot.loader.efi.canTouchEfiVariables = false;
 
   # Use latest kernel.
-  boot.kernelPackages = pkgs.linuxPackages_6_18;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 
   boot.kernelParams = [ "nvidia-drm.modeset=1" ];
 
@@ -241,7 +241,17 @@ in
 
   nvidiaSettings = true;
 
-  package = config.boot.kernelPackages.nvidiaPackages.stable;
+  # package = config.boot.kernelPackages.nvidiaPackages.stable;
+  package = 
+    config.boot.kernelPackages.nvidiaPackages.mkDriver {
+      version = "580.126.18";
+      sha256_64bit = "sha256-p3gbLhwtZcZYCRTHbnntRU0ClF34RxHAMwcKCSqatJ0="; 
+      sha256_aarch64 = "sha256-pruxWQlLurymRL7PbR24NA6dNowwwX35p6j9mBIDcNs=";
+      openSha256 = "sha256-1Q2wuDdZ6KiA/2L3IDN4WXF8t63V/4+JfrFeADI1Cjg=";
+      settingsSha256 = "sha256-QMx4rUPEGp/8Mc+Bd8UmIet/Qr0GY8bnT/oDN8GAoEI=";
+      persistencedSha256 = lib.fakeSha256;
+    };
+  
   };
 
   hardware.nvidia.prime = {

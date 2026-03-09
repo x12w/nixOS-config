1a091529b2cfbf99a59da2c9227541f64d9bd207
x12w <w17802627260@gmail.com>
2026-03-09 23:21:00 +0800

    feat(home):配置nixvim

--- home/programs/enable_and_config/nixvim/default.nix
+++ home/programs/enable_and_config/nixvim/default.nix
@@ -29,7 +29,53 @@
       web-devicons.enable = true;
       lualine.enable = true;
       bufferline.enable = true;
-      nvim-tree.enable = true;
+      bufdelete.enable = true;
+      nvim-tree.enable = false;
+
+      toggleterm = {
+        enable = true;
+        settings = {
+          # 快捷键：Ctrl + \ 或者你自己喜欢的，比如 Meta + t
+          open_mapping = "[[<C-\\>]]";
+          direction = "horizontal";
+          size = 15;
+          float_opts = {
+            border = "curved"; # 圆角边框，匹配 Catppuccin
+          };
+        };
+      };
+
+      neo-tree = {
+        enable = true;
+
+        settings = {
+          enable_git_status = true;
+          enable_diagnostics = true;
+          close_if_last_window = true;
+
+          window = {
+            width = 30;
+            position = "left";
+
+            mapping = {
+              "v" = "openvsplit";
+              "s" = "open_split";
+              "t" = "open_tabnew";
+            };
+          };
+
+          filesystem = {
+            follow_current_file = {
+              enabled = true;
+            };
+            filtered_items = {
+              visible = true;
+              hide_dotfiles = false;
+            };
+          };
+        };
+      };
+
       telescope.enable = true;
 
       # 快速双击 j 退出插入模式
@@ -113,12 +159,61 @@
       };
     }; # plugins 结束
 
+    globals.mapleader = " ";
+
     # --- 常用快捷键 (keymaps 属于 programs.nixvim 的直接子项) ---
     keymaps = [
-      { mode = "n"; key = "<leader>e"; action = "<cmd>NvimTreeToggle<CR>"; }
-      { mode = "n"; key = "<leader>ff"; action = "<cmd>Telescope find_files<CR>"; }
-      { mode = "n"; key = "gd"; action = "lua vim.lsp.buf.definition()"; }
-      { mode = "n"; key = "K"; action = "lua vim.lsp.buf.hover()"; }
+      {
+        mode = "n";
+        key = "<leader>e";
+        action = "<cmd>Neotree toggle<CR>";
+      }
+      {
+        mode = "n";
+        key = "<leader>f";
+        action = "<cmd>Neotree reveal<CR>";
+      }
+      {
+        mode = "n";
+        key = "<leader>ff";
+        action = "<cmd>Telescope find_files<CR>";
+      }
+      {
+        mode = "n";
+        key = "gd";
+        action = "lua vim.lsp.buf.definition()";
+      }
+      {
+        mode = "n";
+        key = "K";
+        action = "lua vim.lsp.buf.hover()";
+      }
+
+      {
+        mode = "n";
+        key = "L"; # 注意是大写 L
+        action = "<cmd>bnext<CR>";
+        options.desc = "下一个标签页";
+      }
+      # 使用 Shift + h 切换到上一个标签 (Buffer)
+      {
+        mode = "n";
+        key = "H"; # 注意是大写 H
+        action = "<cmd>bprevious<CR>";
+        options.desc = "上一个标签页";
+      }
+      # 关闭当前标签页 (Buffer)
+      {
+        mode = "n";
+        key = "<leader>x";
+        action = "<cmd>Bdelete<CR>";
+        options.desc = "关闭当前标签页";
+      }
+      {
+        mode = "t";
+        key = "<Esc>";
+        action = "<C-\\><C-n>";
+      }
     ];
   }; # programs.nixvim 结束
 } # 整个大括号结束

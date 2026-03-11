afa76b410256e9238062a233bcd91d2b66de8bcf
x12w <w17802627260@gmail.com>
2026-03-11 18:35:17 +0800

    feat(home:programs:nixvim):配置报错显示

--- home/programs/enable_and_config/nixvim/default.nix
+++ home/programs/enable_and_config/nixvim/default.nix
@@ -25,6 +25,17 @@
       clipboard = "unnamedplus";
     };
 
+    diagnostic.settings = {
+      # 在行末直接显示错误信息
+      virtual_text = true;
+      # 在错误位置显示波浪线/高亮
+      underline = true;
+      # 更新频率（配合上面的 updatetime）
+      update_in_insert = false;
+      # 配置错误图标
+      severity_sort = true;
+    };
+
     # --- 皮肤与视觉 ---
     colorschemes.catppuccin = {
       enable = true;

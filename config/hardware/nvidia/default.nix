2a8b7f92f40421b209b81e3647c6ba69be2a83eb
x12w <w17802627260@gmail.com>
2026-03-13 21:24:20 +0800

    feat(hardware:nvidia):更新nvidia显卡驱动

--- config/hardware/nvidia/default.nix
+++ config/hardware/nvidia/default.nix
@@ -1,4 +1,9 @@
-{ pkgs, lib, config, ... }:
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
 
 {
   hardware.graphics = {
@@ -13,33 +18,32 @@
 
   hardware.nvidia = {
 
-  modesetting.enable = true;
+    modesetting.enable = true;
 
-  powerManagement.enable = true;
+    powerManagement.enable = true;
 
-  powerManagement.finegrained = false;
+    powerManagement.finegrained = false;
 
-  open = true;
+    open = true;
 
-  nvidiaSettings = true;
+    nvidiaSettings = true;
 
-  # package = config.boot.kernelPackages.nvidiaPackages.stable;
-  package =
-    config.boot.kernelPackages.nvidiaPackages.mkDriver {
-      version = "580.126.18";
-      sha256_64bit = "sha256-p3gbLhwtZcZYCRTHbnntRU0ClF34RxHAMwcKCSqatJ0=";
-      sha256_aarch64 = "sha256-pruxWQlLurymRL7PbR24NA6dNowwwX35p6j9mBIDcNs=";
-      openSha256 = "sha256-1Q2wuDdZ6KiA/2L3IDN4WXF8t63V/4+JfrFeADI1Cjg=";
-      settingsSha256 = "sha256-QMx4rUPEGp/8Mc+Bd8UmIet/Qr0GY8bnT/oDN8GAoEI=";
+    # package = config.boot.kernelPackages.nvidiaPackages.stable;
+    package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+      version = "580.142";
+      sha256_64bit = "sha256-IJFfzz/+icNVDPk7YKBKKFRTFQ2S4kaOGRGkNiBEdWM=";
+      sha256_aarch64 = lib.fakeHash;
+      openSha256 = "sha256-v968LbRqy8jB9+yHy9ceP2TDdgyqfDQ6P41NsCoM2AY=";
+      settingsSha256 = "sha256-BnrIlj5AvXTfqg/qcBt2OS9bTDDZd3uhf5jqOtTMTQM=";
       persistencedSha256 = lib.fakeSha256;
     };
 
   };
 
   hardware.nvidia.prime = {
-  nvidiaBusId = "PCI:1:0:0";
-  amdgpuBusId = "PCI:0:2:0";
+    nvidiaBusId = "PCI:1:0:0";
+    amdgpuBusId = "PCI:0:2:0";
 
-  sync.enable = true;
+    sync.enable = true;
   };
 }

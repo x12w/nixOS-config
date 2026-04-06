99b7295a0cd196a4030ffb8a199366fb20f56817
x12w <w17802627260@gmail.com>
2026-04-06 21:06:14 +0800

    feat:切换到官方显卡驱动

--- config/hardware/nvidia/default.nix
+++ config/hardware/nvidia/default.nix
@@ -28,15 +28,17 @@
 
     nvidiaSettings = true;
 
-    # package = config.boot.kernelPackages.nvidiaPackages.stable;
-    package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-      version = "580.142";
-      sha256_64bit = "sha256-IJFfzz/+icNVDPk7YKBKKFRTFQ2S4kaOGRGkNiBEdWM=";
-      sha256_aarch64 = lib.fakeHash;
-      openSha256 = "sha256-v968LbRqy8jB9+yHy9ceP2TDdgyqfDQ6P41NsCoM2AY=";
-      settingsSha256 = "sha256-BnrIlj5AvXTfqg/qcBt2OS9bTDDZd3uhf5jqOtTMTQM=";
-      persistencedSha256 = lib.fakeSha256;
-    };
+    package = config.boot.kernelPackages.nvidiaPackages.latest;
+    /*
+      package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+        version = "580.142";
+        sha256_64bit = "sha256-IJFfzz/+icNVDPk7YKBKKFRTFQ2S4kaOGRGkNiBEdWM=";
+        sha256_aarch64 = lib.fakeHash;
+        openSha256 = "sha256-v968LbRqy8jB9+yHy9ceP2TDdgyqfDQ6P41NsCoM2AY=";
+        settingsSha256 = "sha256-BnrIlj5AvXTfqg/qcBt2OS9bTDDZd3uhf5jqOtTMTQM=";
+        persistencedSha256 = lib.fakeSha256;
+      };
+    */
 
   };
 
